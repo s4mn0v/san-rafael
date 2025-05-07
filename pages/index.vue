09f5f7c1fe8e42b87f4c0b53d235ee40add6dc79
s4mn0v <s4m.n0v@gmail.com>
2025-05-06 20:40:04 -0500

    Page setup base

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun'" variant="ghost" aria-label="Theme"
+      @click="isDark = !isDark" size="xl" class="p-3 cursor-pointer" title="Cambiar Tema">
+    </UButton>
+    {{ colorMode.value }}
+    <template #fallback>
+      <div class="w-12 h-12" />
+    </template>
+  </ClientOnly>
+</template>
