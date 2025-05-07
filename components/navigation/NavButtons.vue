60372e8a941265510f19e57ff5bd2c67a7558ff0
s4mn0v <s4m.n0v@gmail.com>
2025-05-06 23:11:31 -0500

    Sidebar responsive for desktop view

--- components/navigation/NavButtons.vue
+++ components/navigation/NavButtons.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- v-bind="$attrs" : Pasa automáticamente props y eventos no definidos (como color, size, @click) al botón -->
   <UButton
-    class="gap-2 cursor-pointer hover:bg-[var(--color-custom-50)] hover:text-[var(--color-custom-500)] dark:hover:text-[var(--color-custom-50)] dark:hover:bg-[var(--color-custom-500)] w-full my-2" v-bind="$attrs">
+    class="px-4 py-2 gap-4 cursor-pointer hover:bg-[var(--color-custom-50)] hover:text-[var(--color-custom-500)] dark:hover:text-[var(--color-custom-50)] dark:hover:bg-[var(--color-custom-500)] w-full"
+    v-bind="$attrs">
     <UIcon :name="icon" class="size-6" />
-    <span class="uppercase font-bold tracking-widest">{{ text }}</span>
+    <span class="hidden md:block uppercase font-bold tracking-widest">{{ text }}</span>
   </UButton>
 </template>
 
